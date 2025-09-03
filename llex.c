a64f4f416a8bca2a76e8ce057d6a63978b3e6219
Izak Halseide <ihals@colostate.edu>
2025-09-02 23:19:34 -0600

    Syntax: remove keyword THEN

--- llex.c
+++ llex.c
@@ -41,7 +41,7 @@ static const char *const luaX_tokens [] = {
     "and", "break", "do", "else", "elseif",
     "end", "false", "for", "function", "goto", "if",
     "in", "local", "nil", "not", "or", "repeat",
-    "return", "then", "true", "until", "while",
+    "return", "true", "until", "while",
     "//", "..", "...", "==", ">=", "<=", "~=",
     "<<", ">>", "::", "<eof>",
     "<number>", "<integer>", "<name>", "<string>"
